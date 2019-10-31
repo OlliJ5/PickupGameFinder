d1816a4a3aad218a3b10dbaca9c6b539986d8cdd
Olli Rousu <olli.rousu@hotmail.com>
2019-10-31 12:52:30 +0200

    logging in from frontend

--- pick-up-game-finder/src/App.js
+++ pick-up-game-finder/src/App.js
@@ -1,14 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import gameService from './services/games'
+import loginService from './services/login'
 import Map from './components/Map'
 import { Button } from 'semantic-ui-react'
 import axios from 'axios'
 
 
-function App() {
+const App = () => {
   const [games, setGames] = useState([])
   //const [user, setUser] = useState(null)
   const [location, setLocation] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+
+      setUser(user)
+      window.localStorage.setItem('loggedInUser', JSON.stringify(user))
+      gameService.setToken(user.token)
+      setPassword('')
+      setUsername('')
+    } catch (exception) {
+      console.log('password or username incorrect')
+      setPassword('')
+      setUsername('')
+    }
+  }
 
   const ipLookUp = async () => {
     const res = await axios.get('http://ip-api.com/json')
@@ -20,6 +41,16 @@ function App() {
       .then(res => {
         setGames(res)
       })
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedInUser')
+
+    if(loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      gameService.setToken(user.token)
+    }
   }, [])
 
   useEffect(() => {
@@ -58,12 +89,36 @@ function App() {
     }
   }, [])
 
+  if (user === null) {
+    return (
+      <div>
+        <p>log in</p>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              name="password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    )
+  }
   return (
     <div>
-      {games.map(game =>
-        <h2 key={game.id}>
-          {game.desc}
-        </h2>)}
       <Button primary>Create a game</Button>
       <Map games={games} location={location} />
     </div>
--- pick-up-game-finder/src/services/games.js
+++ pick-up-game-finder/src/services/games.js
@@ -2,11 +2,28 @@ import axios from 'axios'
 
 const baseUrl = '/api/games'
 
+let token = null
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`
+}
+
 const getAllActive = () => {
   const request = axios.get(`${baseUrl}/active`)
   return request.then(res => res.data)
 }
 
+const create = async (newGame) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+
+  const response = await axios.post(baseUrl, newGame, config)
+  return response.data
+}
+
 export default {
-  getAllActive
+  getAllActive,
+  create,
+  setToken
 }
