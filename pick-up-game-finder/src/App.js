6a1f2a61db483d9c4335134e3dee41e4a97c2648
Olli Rousu <olli.rousu@hotmail.com>
2019-11-01 15:06:14 +0200

    account creation from frontend

--- pick-up-game-finder/src/App.js
+++ pick-up-game-finder/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import gameService from './services/games'
 import playerService from './services/players'
+import userService from './services/users'
 import loginService from './services/login'
 import Map from './components/Map'
 import axios from 'axios'
@@ -10,12 +11,20 @@ const App = () => {
   const [games, setGames] = useState([])
   //const [user, setUser] = useState(null)
   const [location, setLocation] = useState(null)
+
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+
   const [user, setUser] = useState(null)
+
   const [duration, setDuration] = useState(1)
   const [desc, setDesc] = useState('')
   const [maxParticipants, setmaxParticipants] = useState(10)
+
+  const [newUsername, setNewUsername] = useState('')
+  const [newName, setNewName] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -51,6 +60,24 @@ const App = () => {
       setDesc('')
       setmaxParticipants(10)
     }catch(exception) {
+      console.log('something went wrong')
+    }
+  }
+
+  const createAccount = async (event) => {
+    event.preventDefault()
+    try {
+      const newUser = {
+        username: newUsername,
+        name: newName,
+        password: newPassword
+      }
+      const user = await userService.create(newUser)
+      console.log('luotiin', user)
+      setNewUsername('')
+      setNewName('')
+      setNewPassword('')
+    } catch(exception) {
       console.log('something went wrong')
     }
   }
@@ -138,6 +165,37 @@ const App = () => {
             />
           </div>
           <button type="submit">login</button>
+        </form>
+        <p>or create an account</p>
+        <form onSubmit={createAccount}>
+          <div>
+          username
+            <input
+              type="text"
+              value={newUsername}
+              name="newUsername"
+              onChange={({ target }) => setNewUsername(target.value)}
+            />
+          </div>
+          <div>
+            name
+            <input
+              type="text"
+              value={newName}
+              name="name"
+              onChange={({ target }) => setNewName(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={newPassword}
+              name="newPassword"
+              onChange={({ target }) => setNewPassword(target.value)}
+            />
+          </div>
+          <button type="submit">create an account</button>
         </form>
       </div>
     )
