a5d64a7fc5f1d9034cc4b0c9256c7c610e3bf5af
Olli Rousu <olli.rousu@hotmail.com>
2020-01-29 15:12:18 +0200

    Updates

---
+++ precache-manifest.ade01e9ea54b6e12f09dd835fa868d38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59704c8f3522a779c637804420f1a047",
+    "url": "/PickupGameFinder/index.html"
+  },
+  {
+    "revision": "4b7e4c96e96e6d7841e2",
+    "url": "/PickupGameFinder/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "4b7e4c96e96e6d7841e2",
+    "url": "/PickupGameFinder/static/js/2.10d4a50d.chunk.js"
+  },
+  {
+    "revision": "1ed63ca8f8dfac6871b2",
+    "url": "/PickupGameFinder/static/js/main.02aeeab9.chunk.js"
+  },
+  {
+    "revision": "819b278a9b50ed9dd9d1",
+    "url": "/PickupGameFinder/static/js/runtime~main.0619d0fb.js"
+  },
+  {
+    "revision": "4b4fdbacdb34aa06ae879d77e3936019",
+    "url": "/PickupGameFinder/static/media/kobe.4b4fdbac.jpg"
+  }
+]);
