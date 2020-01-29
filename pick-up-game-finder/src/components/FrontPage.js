8d3f0314cec8a339eda3319b754216eb75cedcaa
Olli Rousu <olli.rousu@hotmail.com>
2020-01-29 15:00:21 +0200

    picture of kobe

--- pick-up-game-finder/src/components/FrontPage.js
+++ pick-up-game-finder/src/components/FrontPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Grid, Header } from 'semantic-ui-react'
 import NewAccountForm from './forms/NewAccountForm'
 import LoginForm from './forms/LoginForm'
+import kobe from '../kobe.jpg'
 
 const FrontPage = () => {
   return (
@@ -21,6 +22,7 @@ const FrontPage = () => {
               Find local pickupgames and start balling
             </Header.Subheader>
           </Header>
+          <img src={kobe} alt="Basketball legend Kobe Bryant" style={{maxWidth:'60%', maxHeight:'60%'}}/>
         </Grid.Column>
         <Grid.Column>
           <NewAccountForm />
