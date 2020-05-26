805ac62293878d1421ff26605b15e4d45450f50d
Olli Rousu <olli.rousu@hotmail.com>
2020-05-26 11:18:04 +0300

    basic carousel inside a modal

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,33 +1,35 @@
 import React from 'react'
-import { Image, Modal } from 'semantic-ui-react'
-import { CarouselProvider, Slider, Slide } from 'pure-react-carousel'
+import { Image, Modal, Divider, Button, Container } from 'semantic-ui-react'
+import { CarouselProvider, Slider, Slide, ButtonNext, ButtonBack, Dot } from 'pure-react-carousel'
 import 'pure-react-carousel/dist/react-carousel.es.css'
 import intro1 from '../resources/intro/creategames.png'
 import intro2 from '../resources/intro/createdgame.png'
 import intro3 from '../resources/intro/joingames.png'
 
-
 const Intro = () => {
   return (
-    <Modal defaultOpen={true}>
+    <Modal basic defaultOpen={true}>
       <CarouselProvider
         naturalSlideWidth={1}
-        naturalSlideHeight={1}
+        naturalSlideHeight={0.5}
         totalSlides={3}
       >
         <Slider>
-          <Slide tag="a" index={0}>
-            <Image src={intro1} />
+          <Slide index={0}>
+            <Image wrapped src={intro1} />
           </Slide>
-          <Slide tag="a" index={1}>
-            <Image src={intro2} />
+          <Slide index={1}>
+            <Image wrapped src={intro2} />
           </Slide>
-          <Slide tag="a" index={2}>
-            <Image src={intro3} />
+          <Slide index={2}>
+            <Image wrapped src={intro3} />
           </Slide>
         </Slider>
+        <div style={{ textAlign: 'center' }}>
+          <ButtonBack>BACK</ButtonBack>
+          <ButtonNext>NEXT</ButtonNext>
+        </div>
       </CarouselProvider>
-
     </Modal>
   )
 }
