caa398e7613612266a4fe250c9794bb5ac98f1b7
Olli Rousu <olli.rousu@hotmail.com>
2020-06-04 19:06:53 +0300

    small router improvement

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ const App = (props) => {
             <Profile />
           </div>
         </Route>
-        <Route path='/home'>
+        <Route path=''>
           {props.user.showIntro && (
             <Intro />
           )}
